a631fb01f1138251631495f53f5dfb1c2d24dc01
Ek Araujo Carlos Manuel <112431620+iShadowGG@users.noreply.github.com>
2023-07-26 16:43:55 -0500

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -3,7 +3,7 @@ sudo apt-get update -y
 sudo apt-get install -y nginx
 
 #move into html dir
-cd /var/www/html
+cd /var/www/
 
 #mirror demo page from s3 bucket
 sudo wget --mirror --page-requisites --convert-links --no-clobber http://example-bucket-archie-indexpage-azure1.s3-website-eu-west-1.amazonaws.com
@@ -17,7 +17,7 @@ sudo mv example-bucket-archie-indexpage-azure1.s3-website-eu-west-1.amazonaws.co
 #give permissions to execute the script and move it the html folder
 sudo chmod 755 script.sh
 sudo mv script.sh html/
-cd /var/www/html/html
+cd /var/www/html/
 
 #start bootstrap script which calls variable and changes index.html information
 sudo ./script.sh      
